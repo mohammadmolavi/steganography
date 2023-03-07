e189b5d9eee455d6b9a889106f557fd1e3fe6e69
mohammadmolavi <mohammadmolavi97@gmail.com>
2023-03-08 00:31:08 +0330

    whole of project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Steganography
+## hide white and black picture in a full hd colorful picture
+### for example:
+![pic1](https://s2.uupload.ir/files/pic1_5b3a.jpg)
+### is the colorful picture and we must hide below picture in this
+![pic2](https://s2.uupload.ir/files/pic2_ppbz.jpg)
+### The photo below is a photo in which the black and white photo is hidden
+![encrypted](https://s2.uupload.ir/files/encrypted_0cl5.png)
+### then this app decrypted the white and black photo
+![decrypted](https://s2.uupload.ir/files/decrypted_ynqj.png)
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import random
+
+
+def encrypt():
+    random.seed(10)
+    img1 = cv2.imread('pic1.jpg')
+    img2 = cv2.imread('pic2.jpg')
+    garyimage = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(garyimage, 127, 255, cv2.THRESH_BINARY)
+    for i in range(blackAndWhiteImage.shape[0]):
+        for j in range(blackAndWhiteImage.shape[1]):
+            x, y, z = int(random.random()*100000), int(random.random()*100000), int(random.random()*100000)
+
+            xx , yy  , zz = img1.shape
+            x %= xx
+            y %= yy
+            z %= zz
+            if blackAndWhiteImage[i][j] % 2 == 0:
+                if img1[x][y][z] % 2 == 1:
+                    img1[x][y][z] -= 1
+            else:
+                if img1[x][y][z] % 2 == 0:
+                    img1[x][y][z] += 1
+    cv2.imshow('encrypted' , img1)
+    cv2.waitKey(0)
+    cv2.imwrite("encrypted.png" , img1)
+
+
+def decrypt():
+    random.seed(10)
+    encrypted = cv2.imread("encrypted.png")
+    kir = cv2.imread("pic2.jpg")
+    xx , yy , zz = kir.shape
+    xxx,yyy,zzz=encrypted.shape
+    decrypted = np.zeros((xx , yy) , dtype=np.uint8)
+    for i in range(xx):
+        for j in range(yy):
+            x, y, z = int(random.random()*100000), int(random.random()*100000), int(random.random()*100000)
+            x %= xxx
+            y %= yyy
+            z %= 3
+            if encrypted[x][y][z] % 2 == 1:
+                decrypted[i][j] = 255
+    cv2.imshow('decrypted' , decrypted)
+    cv2.waitKey(0)
+    cv2.imwrite("decrypted.png" , decrypted)
+
+
+
+encrypt()
+decrypt()
